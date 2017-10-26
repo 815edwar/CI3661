466f06feb9cbca709722ae326d9aed4b98248196
Ale Marrero <alemarrero16@gmail.com>
2017-10-25 20:49:58 -0400

    predecir

--- Proyecto1/Haskinator.hs
+++ Proyecto1/Haskinator.hs
@@ -44,31 +44,62 @@ cargar = do
             putStrLn ""
             return . crearOraculo $ ""
 
--- insertarPregunta 
-
--- predecir :: Oraculo -> IO Oraculo
--- predecir (Prediccion ps) = do
---     putStrLn "Estoy pensando en......"
---     putStrLn ps
---     putStrLn "¿Estoy en lo correcto?"
---     putStrLn "Si es así escribe si, de lo contrario escribe no."
---     resp <- getLine
---     case toLower resp of
---          "si" -> return o
---          "no" -> do
---             putStrLn "¡Vaya! Me he equivocado. "
---             putStrLn "¿Cuál es la predicción correcta?"
---             prediccion <- getLine
---             putStrLn ("¿Qué pregunta lo distingue de " ++ ps)
---             pregunta <- getLine
---             putStrLn ("¿Cuál es la respuesta que lleva a " ++ prediccion)
---             opcion1 <- getLine
---             putStrLn ("¿Cuál es la respuesta que lleva a " ++ ps)
---             opcion2 <- getLine
---             let qs = ramificar [opcion1, opcion2] 
-
-
-
+predecir :: Oraculo -> IO Oraculo
+predecir o@(Prediccion ps) = do
+    putStrLn "Estoy pensando en......"
+    putStrLn ps
+    putStrLn "¿Estoy en lo correcto?"
+    putStrLn "Si es así escribe si, de lo contrario escribe no."
+    resp <- getLine
+    case resp of
+         "si" -> do
+            putStrLn ""
+            putStrLn "¡Sí! He triunfado."
+            return o
+         "no" -> do
+            putStrLn "¡Vaya! Me he equivocado. "
+            putStrLn "¿Cuál es la predicción correcta?"
+            prediccion <- getLine
+            let o' = Prediccion prediccion
+            putStrLn ("¿Qué pregunta lo distingue de " ++ ps ++ "?")
+            pregunta <- getLine
+            putStrLn ("¿Cuál es la respuesta que lleva a " ++ prediccion ++ "?")
+            opcion1 <- getLine
+            putStrLn ("¿Cuál es la respuesta que lleva a " ++ ps ++ "?")
+            opcion2 <- getLine
+            let nuevoOra = ramificar [opcion2, opcion1] [o, o'] pregunta
+            return nuevoOra
+         otherwise -> do
+            putStrLn "Escribiste una opción incorrecta, vuelve a intentarlo."
+            return o
+predecir o@(Pregunta qs os) = do
+    putStrLn qs  
+    mapM_ putStr (map (++" / ") (M.keys (opciones(o)))) 
+    putStrLn " ninguna"
+    resp <- getLine
+    case resp of
+        "ninguna" -> do
+            putStrLn "¡Vaya!...¿Qué opción esperabas?"
+            opcion <- getLine
+            putStrLn "Ya que no sabía de la existencia de esa opción, ¿podrías decirme en qué pensaste?"
+            ps <- getLine
+            let predNueva = Prediccion ps 
+            let listaOp = opcion:(M.keys(opciones (o)))
+            let predicciones = predNueva:(M.elems(opciones (o)))
+            let nuevoOra = ramificar (listaOp) (predicciones) qs 
+            return nuevoOra
+        
+        otherwise -> do 
+            if not (resp `elem` M.keys(opciones (o))) 
+                then do 
+                    putStrLn "Lo que introduciste no es una opción válida. Vuelve a intentar"
+                    return o
+            else do 
+                nuevoOra <- predecir (respuesta(o) resp)
+                let listaOp = M.insert resp nuevoOra os
+                .0
+                let oraculoNuevo = Pregunta qs listaOp
+                return oraculoNuevo
 
 pedirOpcion :: Oraculo -> IO ()
 pedirOpcion o = do
@@ -92,8 +123,9 @@ pedirOpcion o = do
             o <- pedirNuevaPrediccion
             pedirOpcion o
          "2" -> do
-            putStrLn "Aqui se ejecuta funcion de opcion 2"
-            pedirOpcion o
+            o' <- predecir o
+            putStrLn ""
+            pedirOpcion o'
          "3" -> do
             persistir o
             putStrLn "¡Se ha guardado el oráculo en el archivo correctamente!"
@@ -101,8 +133,7 @@ pedirOpcion o = do
             pedirOpcion o
          "4" -> do
             o <- cargar
-            print o
-            putStrLn "¡Se ha leido el oráculo en el archivo correctamente"
+            putStrLn "¡Se ha leido el oráculo en el archivo correctamente!"
             putStrLn ""
             pedirOpcion o
          "5" -> do
